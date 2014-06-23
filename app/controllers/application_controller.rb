20c4058099e2cd1ca640e30f6aae63932df5b3f8
SuprajaSuresh <supraja.suresh.s@gmail.com>
2014-06-23 22:39:02 +0530

    Fixed styling issues and added discount on ticket fee

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   include SessionsHelper
-
-  def login_required
-    redirect_to root_path, :alert => 'Please login to attend the event.' unless signed_in?
-  end
-
 end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -5,10 +5,18 @@ module EventsHelper
 	end
 	
 	def format_time(event)
-		event.date_and_time.strftime('%H:%M')
+		event.date_and_time.strftime('%H:%M %p')
+	end
+
+	def discounted_rate(event)
+		event.ticket_fee - (event.ticket_fee * 5/100)
 	end
 
   def calculate_fee(event,user)
-    event.ticket_fee
+  	unless user.nil?
+  		user.gender == "Female" ? discounted_rate(event) : event.ticket_fee
+  	else
+  		event.ticket_fee
+  	end
   end
 end
